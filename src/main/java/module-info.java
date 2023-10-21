bda91b055415ffa49a77026557e530ec8052f394
TeslaC00 <vikaskumar16122002@gmail.com>
2023-10-21 15:08:01 +0530

    added objectMapper module

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,8 @@
 module com.metro.prediction.metroroutepredictor {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires com.fasterxml.jackson.databind;
+    requires com.fasterxml.jackson.core;
 
     opens com.metro.prediction.metroroutepredictor to javafx.fxml;
     exports com.metro.prediction.metroroutepredictor;
