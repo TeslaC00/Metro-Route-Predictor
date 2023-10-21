de4e2d93596f983236f14e8465ed0e363374300a
TeslaC00 <vikaskumar16122002@gmail.com>
2023-10-21 15:08:36 +0530

    added map canvas

--- src/main/java/com/metro/prediction/metroroutepredictor/Main.java
+++ src/main/java/com/metro/prediction/metroroutepredictor/Main.java
@@ -1,5 +1,6 @@
 package com.metro.prediction.metroroutepredictor;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -8,6 +9,10 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class Main extends Application {
+    public static void main(String[] args) {
+        launch();
+    }
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("main.fxml"));
@@ -15,9 +20,6 @@ public class Main extends Application {
         stage.setTitle("Metro Route Predictor");
         stage.setScene(scene);
         stage.show();
-    }
-
-    public static void main(String[] args) {
-        launch();
+        ObjectMapper objectMapper = new ObjectMapper();
     }
 }
--- src/main/java/com/metro/prediction/metroroutepredictor/MainController.java
+++ src/main/java/com/metro/prediction/metroroutepredictor/MainController.java
@@ -1,14 +1,31 @@
 package com.metro.prediction.metroroutepredictor;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.Initializable;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.ComboBox;
 
-public class MainController {
-    @FXML
-    private Label welcomeText;
+import java.net.URL;
+import java.util.ResourceBundle;
 
+public class MainController implements Initializable {
+    String[] stations = {"Station 1", "Station 2", "Station 3"};
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private ComboBox<String> startStation;
+    @FXML
+    private ComboBox<String> stopStation;
+    @FXML
+    private Canvas metroMap;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        startStation.getItems().addAll(stations);
+        stopStation.getItems().addAll(stations);
+        drawMap();
+    }
+
+    private void drawMap() {
+        GraphicsContext gc = metroMap.getGraphicsContext2D();
     }
 }
