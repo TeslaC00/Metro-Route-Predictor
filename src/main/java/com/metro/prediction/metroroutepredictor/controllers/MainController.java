8b851c83ca962e13028cdea24a003a877eb25bc8
TeslaC00 <vikaskumar16122002@gmail.com>
2023-11-05 20:54:33 +0530

    added alerts

--- src/main/java/com/metro/prediction/metroroutepredictor/controllers/MainController.java
+++ src/main/java/com/metro/prediction/metroroutepredictor/controllers/MainController.java
@@ -1,23 +1,84 @@
 package com.metro.prediction.metroroutepredictor.controllers;
 
 import com.metro.prediction.metroroutepredictor.model.classes.Model;
-import com.metro.prediction.metroroutepredictor.model.interfaces.Connection;
 import com.metro.prediction.metroroutepredictor.model.interfaces.Station;
 import javafx.fxml.Initializable;
+import javafx.scene.control.*;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class MainController implements Initializable {
+    public ComboBox<Station> startStationComboBox;
+    public ComboBox<Station> endStationComboBox;
+    public RadioButton comfyRadioButton;
+    public RadioButton shortRadioButton;
+    public RadioButton cheapRadioButton;
+    public Button findRouteButton;
+    public ToggleGroup routePropertyToggleGroup;
+    public Label testLabel;
+    public ListView routeListView;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        testMetroDataLoader();
+        initializeComboBox();
+        initializeFindRouteButton();
     }
 
-    private void testMetroDataLoader() {
-        Station station = Model.getInstance().getAllStations().get(0);
-        Connection connection = Model.getInstance().getAllConnections().get(0);
-        System.out.println("Name: " + station.getStationName() + " x: " + station.getX() + " y: " + station.getY());
-        System.out.println("src: " + connection.getSource() + " dst: " + connection.getDestination() + " color: " + connection.getColor() + " distance: " + connection.getDistance() + " time: " + connection.getTimeInSeconds() + " route name: " + connection.getRouteName());
+    private void initializeFindRouteButton() {
+        findRouteButton.setOnAction(event -> findRoute());
     }
+
+    private void initializeComboBox() {
+        List<Station> sortedStations = new ArrayList<>(Model.getInstance().getAllStations());
+        sortedStations.sort(Comparator.comparing(Station::getStationName));
+        startStationComboBox.getItems().addAll(sortedStations);
+        endStationComboBox.getItems().addAll(sortedStations);
+    }
+
+    private void findRoute() {
+        if (startStationComboBox.getValue() == null) {
+            showErrorAlert("Null Station Error", "No Start Station is selected.\nPlease select a starting station");
+            return;
+        }
+
+        if (endStationComboBox.getValue() == null) {
+            showErrorAlert("Null Station Error", "No End Station is selected.\nPlease select a ending station");
+            return;
+        }
+        if (startStationComboBox.getValue().equals(endStationComboBox.getValue())) {
+            showWarnAlert("Similar Values", "You have selected same station for Starting and ending.\nPLease select different stations");
+            return;
+        }
+
+        if (comfyRadioButton.isSelected()) {
+            testLabel.setText("Finding Comfy route from: " + startStationComboBox.getValue() + " to " + endStationComboBox.getValue());
+        } else if (shortRadioButton.isSelected()) {
+            testLabel.setText("Finding Shortest route from: " + startStationComboBox.getValue() + " to " + endStationComboBox.getValue());
+        } else if (cheapRadioButton.isSelected()) {
+            testLabel.setText("Finding Cheapest route from: " + startStationComboBox.getValue() + " to " + endStationComboBox.getValue());
+        } else {
+            showErrorAlert("Route Property Error", "No route property is selected.\nSelect either of the options:\nLess Interchange\tShortest\tCheapest");
+        }
+    }
+
+    private void showWarnAlert(String title, String content) {
+        Alert alert = new Alert(Alert.AlertType.WARNING);
+        alert.setTitle("Warning");
+        alert.setHeaderText(title);
+        alert.setContentText(content);
+        alert.showAndWait();
+    }
+
+    private void showErrorAlert(String title, String content) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Error");
+        alert.setHeaderText(title);
+        alert.setContentText(content);
+        alert.showAndWait();
+    }
+
 }
--- src/main/java/com/metro/prediction/metroroutepredictor/views/ViewFactory.java
+++ src/main/java/com/metro/prediction/metroroutepredictor/views/ViewFactory.java
@@ -1,4 +1,29 @@
 package com.metro.prediction.metroroutepredictor.views;
 
+import com.metro.prediction.metroroutepredictor.model.exceptions.FxmlLoadingException;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
+
 public class ViewFactory {
+
+    public void showMainWindow() {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/Fxml/Main.fxml"));
+        Scene scene = null;
+        try {
+            scene = new Scene(loader.load());
+        } catch (IOException e) {
+            throw new FxmlLoadingException(e);
+        }
+        Stage stage = new Stage();
+        stage.setTitle("Metro Route Predictor");
+        stage.getIcons().add(new Image(Objects.requireNonNull(getClass().getResourceAsStream("/Images/train_icon.png"))));
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.show();
+    }
 }
